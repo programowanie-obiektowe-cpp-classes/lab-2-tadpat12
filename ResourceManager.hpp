148ff6696ccf79e42f840d5b2a6a055ee51c2de9
tadpat12 <tadpat12@gmail.com>
2024-11-03 19:50:13 +0100

    1

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -2,7 +2,24 @@
 
 #include "Resource.hpp"
 
-class ResourceManager
+class ResourceManager 
 {
-    // Twoja implementacja tutaj
+    public:
+    ResourceManager() : r1(){
+    };
+    ~ResourceManager() = default;
+    ResourceManager (const ResourceManager& rm2) : r1{rm2.r1} {
+    }
+    ResourceManager& operator=(const ResourceManager& rm2){
+        if(this != &rm2){
+            r1 = rm2.r1;
+        }
+        return *this;
+    }
+    double const get()  {
+        return  r1.get();
+    }
+    Resource r1;
 };
+
+
